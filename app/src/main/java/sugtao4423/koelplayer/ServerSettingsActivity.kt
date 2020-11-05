154e852baf9b4c329bcf24dfa1682d06b632b548
sugtao4423 <sugtao4423@gmail.com>
2020-11-06 03:37:46 +0900

    トークンの保存後Activityをfinishさせるように

--- app/src/main/java/sugtao4423/koelplayer/ServerSettingsActivity.kt
+++ app/src/main/java/sugtao4423/koelplayer/ServerSettingsActivity.kt
@@ -1,6 +1,7 @@
 package sugtao4423.koelplayer
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
@@ -38,6 +39,8 @@ class ServerSettingsActivity : AppCompatActivity() {
             }
             Prefs(this@ServerSettingsActivity).koelServer = host
             Prefs(this@ServerSettingsActivity).koelToken = token
+            startActivity(Intent(this@ServerSettingsActivity, MainActivity::class.java))
+            finish()
         }
     }
 
